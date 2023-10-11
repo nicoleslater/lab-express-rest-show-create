03bdef42e2318c7dc5a6811b338e693388418564
Nicole Slater <nicoleslater@pursuit.org>
2023-10-10 21:42:02 -0400

    Updating to pass tests

--- controllers/captainLogsController.js
+++ controllers/captainLogsController.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const captainLogs = express.Router();
 
-const captainLogsData = require("../model/captainsLogs");
+const captainLogsData = require("../model/log");
 
 captainLogs.get("/", (req, res) => {
     console.log("Sending all captain log data")
