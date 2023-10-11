89a793a9551947d2d95a24609342e4fb88ab7f3f
Nicole Slater <nicoleslater@pursuit.org>
2023-10-10 21:49:14 -0400

    Updating captainLogs Controller

--- controllers/captainLogsController.js
+++ controllers/captainLogsController.js
@@ -7,7 +7,27 @@ const captainLogsData = require("../model/log");
 captainLogs.get("/", (req, res) => {
     console.log("Sending all captain log data")
 
-    res.json(captainLogsData)
+    res.json(captainLogsData);
+})
+
+
+captainLogs.get("/:index", (req, res) => {
+    const {index} = req.params;
+    console.log("Show ROUTE!!!!!!!!")
+
+    if (captainLogsData[index]){
+        res.status(200).json(captainLogsData[index])
+    } else {
+        res.status(404).send("No log at that index");
+    }
+})
+
+
+captainLogs.post("/", (req, res) => {
+    console.log("Post Route was HIT!")
+    console.log(req.body, "<<<<<-----")
+    captainLogsData.push(req.body);
+    res.status(200).json({ status: "OK", payload: captainLogsData[captainLogsData.length - 1]})
 })
 
 module.exports = captainLogs;
